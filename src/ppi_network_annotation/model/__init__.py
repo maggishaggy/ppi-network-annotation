29a4f17a0e1042ce1eb3d94a04c119f80e3b23ac
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-10-24 15:27:32 +0200

    Add nicer imports

--- src/ppi_network_annotation/model/__init__.py
+++ src/ppi_network_annotation/model/__init__.py
@@ -1,3 +1,8 @@
 # -*- coding: utf-8 -*-
 
 """Package that includes classes for data models."""
+
+from ppi_network_annotation.model.attribute_network import AttributeNetwork
+from ppi_network_annotation.model.filtered_network import FilteredNetwork
+from ppi_network_annotation.model.labeled_network import LabeledNetwork
+from ppi_network_annotation.model.network import Network
