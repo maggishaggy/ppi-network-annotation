162aa05dafcb042db668043394c39f965b7489d8
Özlem Muslu <ozlemmuslu@gmail.com>
2018-10-25 12:15:28 +0200

    Add bumpversion.cfg and top-level imports

--- src/ppi_network_annotation/__init__.py
+++ src/ppi_network_annotation/__init__.py
@@ -1,7 +1,15 @@
 """Library for annotating a protein protein interaction network with differential gene 
 expression."""
 
-__version__ = '0.0.2.1-dev'
+from ppi_network_annotation.model import AttributeNetwork
+from ppi_network_annotation.model import FilteredNetwork
+from ppi_network_annotation.model import Gene
+from ppi_network_annotation.model import LabeledNetwork
+from ppi_network_annotation.model import Network
+from ppi_network_annotation.pipeline import generate_ppi_network, parse_dge
+from ppi_network_annotation.constants import get_version
+
+__version__ = '0.0.3-dev'
 
 __title__ = 'ppi_network_annotation'
 __description__ = 'Library for annotating a protein protein interaction network with differential ' \
